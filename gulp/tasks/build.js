46b6c0ca0fd652ae5bb3f256c2ad616e18aa462e
Rebecka <rebeckamerkel@gmail.com>
2017-03-23 16:11:35 +0100

    force rename folder

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -42,7 +42,7 @@ gulp.task('optimizeImages', ['deleteDistFolder', 'styles', 'scripts'], function 
             interlaced: true,
             multipass: true
         }))
-        .pipe(gulp.dest("./docs/assets/images"));
+        .pipe(gulp.dest("./docs/assets/images2"));
 });
 
 gulp.task('usemin', ['deleteDistFolder'], function () {
